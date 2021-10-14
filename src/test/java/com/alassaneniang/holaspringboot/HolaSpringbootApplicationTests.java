b52e67eb3c6036d2fc7cb771d5e7bff12fa36a4d
Alassane NIANG <adniang75@gmail.com>
2021-10-14 22:07:33 +0200

    Initial commit

---
+++ src/test/java/com/alassaneniang/holaspringboot/HolaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.holaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
